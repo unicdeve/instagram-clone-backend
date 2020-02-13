045e294959341dba5e7f166b4c37e2919d811007
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-02-13 14:02:41 +0100

    initial commit; rest setups

---
+++ insta_backend/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='Instagram Clone')
+
+urlpatterns = [
+    path('', schema_view),
+    path('admin/', admin.site.urls),
+]
